71c027e566d01793534d6d9c39b1101ae0ffe38b
Onliner044 <39239765+Onliner044@users.noreply.github.com>
2018-10-08 14:18:16 +0300

    1

--- src/Catalogs/VerificationCatalog/actions/actions.js
+++ src/Catalogs/VerificationCatalog/actions/actions.js
@@ -4,7 +4,3 @@ export const visibleUserPanel = (payload) => {
         payload
     }
 }
-
-export const addRecordUser = (email) => {
-    
-}
--- src/Catalogs/VerificationCatalog/components/Log.js
+++ src/Catalogs/VerificationCatalog/components/Log.js
@@ -3,18 +3,17 @@ import React, { Component } from 'react';
 import { login, getTypeSocialNetwork } from '../other/functions';
 import { Verification } from './Verification';
 
-
 export const Log = ({showUserPanel}) => {
   return (
-      <form>
-          <input type="email" autoComplete="true" placeholder="E-mail" ref={(el) => this.email = el}/>
-          <br />
-          <input type="password" placeholder="Пароль" ref={(el) => this.password = el}/>
-          <br />
-          <button onClick={e => onClickLogIn(e, this.email, this.password, showUserPanel)}>
-            Войти
-          </button>
-          <Verification onClick={getTypeSocialNetwork("google")} src="https://yt3.ggpht.com/a-/ACSszfEg5xl_vAi2hBOieBvZSMPax2GhMLlJSeXApg=s900-mo-c-c0xffffffff-rj-k-no" />
+    <form>
+      <input type="email" autoComplete="true" placeholder="E-mail" ref={(el) => this.email = el}/>
+      <br />
+      <input type="password" placeholder="Пароль" ref={(el) => this.password = el}/>
+      <br />
+      <button onClick={e => onClickLogIn(e, this.email, this.password, showUserPanel)}>
+        Войти
+      </button>
+      <Verification onClick={getTypeSocialNetwork("google")} src="https://yt3.ggpht.com/a-/ACSszfEg5xl_vAi2hBOieBvZSMPax2GhMLlJSeXApg=s900-mo-c-c0xffffffff-rj-k-no" />
     </form>
   )
 }
